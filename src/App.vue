db7c137c607a27e686dc08945e1553854ff94209
Seyed M. Afzouni <afzouni@sotoon.ir>
2022-03-26 04:27:15 +0430

    [feat] Implement Meta tags

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
-  <Irancell/>
+  <Irancell />
 </template>
 
 <script>
-import Irancell from './components/Irancell.vue'
+import Irancell from "./components/Irancell.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Irancell
-  }
-}
+    Irancell,
+  },
+  metaInfo() {
+    return {
+      title: "بسته های اینترنتی ایرانسل",
+      meta: [
+        {
+          name: "description",
+          content:
+            "در این صفحه لیست بسته‌های ایرانسل را مشاهده می‌کنید و به شما کمک می‌کند انتخاب بهینه‌ای برای خرید بسته اینترنتی خود داشته باشید.",
+        },
+        {
+          property: "og:title",
+          content: "در این صفحه لیست بسته‌های ایرانسل را مشاهده می‌کنید و به شما کمک می‌کند انتخاب بهینه‌ای برای خرید بسته اینترنتی خود داشته باشید.",
+        },
+        { property: "og:site_name", content: "بسته های اینترنتی ایرانسل" },
+        { property: "og:type", content: "website" },
+        { name: "robots", content: "index,follow" },
+      ],
+    };
+  },
+};
 </script>
 
