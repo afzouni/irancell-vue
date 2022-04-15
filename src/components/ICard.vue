d9e1e9d5c4e038a014f7cb2ded48ab0a2dc2adb1
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2022-04-15 16:37:02 +0430

    [feat]: implement "ICard" component

---
+++ src/components/ICard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="i-card border">
+    <div v-if="title" class="i-card__header">
+      <span class="">
+        {{ title }}
+      </span>
+    </div>
+
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ICard",
+  props: {
+    title: {
+      type: String,
+      required: false,
+      default: '',
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../assets/scss/components/i-card";
+</style>
