fb2aa829e21aad52fff2dd999f25d5785a383fa7
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2022-04-15 12:10:24 +0430

    [feat]: implement "ICheckbox" component

---
+++ src/components/ICheckbox.vue
@@ -0,0 +1,43 @@
+<template>
+  <label>
+    <input
+        type="checkbox"
+        v-model="model"
+        class="filter-attribute-checkbox ib-m"
+    />
+
+    {{ label }}
+  </label>
+</template>
+
+<script>
+export default {
+  name: "ICheckbox",
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Boolean,
+      required: false,
+      default: false,
+    }
+  },
+  data() {
+    return {
+      currentValue: this.value,
+    };
+  },
+  computed: {
+    model: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+      },
+    },
+  },
+}
+</script>
